1d8c747b25804c61c9c10f46a3e0e76dff24cd07
TruDan <trudan97@gmail.com>
2021-03-11 21:02:18 +0100

    text alignment

---
+++ src/RocketUI/Primitive/TextAlignment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RocketUI
+{
+    [Flags]
+    public enum TextAlignment
+    {
+        Left      = 0b000,
+        Center    = 0b010,
+        Right     = 0b100
+    }
+}
