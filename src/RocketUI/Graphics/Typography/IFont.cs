9996980a0dce251245fe95aa4121fa9c0556bccd
TruDan <trudan97@gmail.com>
2021-09-05 19:42:43 +0200

    refactor font interface

--- src/RocketUI/Graphics/Typography/IFont.cs
+++ src/RocketUI/Graphics/Typography/IFont.cs
@@ -9,26 +9,28 @@ namespace RocketUI
         IReadOnlyCollection<char> Characters { get; }
         //IFontGlyph GetGlyphOrDefault(char character);
 
-        Vector2 MeasureString(string text, float scale = 1.0f);
-        Vector2 MeasureString(string text, Vector2 scale);
+        Vector2 MeasureString(string text, float   scale) => MeasureString(text) * scale;
+        Vector2 MeasureString(string text, Vector2 scale) => MeasureString(text) * scale;
+
+        Vector2 MeasureString(string text);
         //void MeasureString(string text, out Vector2 size);
 
-        void DrawString(SpriteBatch   sb, 
-                        string text, 
-                        Vector2 position,
-                        Color     color,
-                        FontStyle     style      = FontStyle.None, Vector2? scale = null,
-                        float         opacity    = 1f,
-                        float         rotation   = 0f, Vector2? origin = null,
-                        SpriteEffects effects    = SpriteEffects.None,
-                        float         layerDepth = 0f);
+        void DrawString(SpriteBatch sb,
+            string                  text,
+            Vector2                 position,
+            Color                   color,
+            FontStyle               style      = FontStyle.None, Vector2? scale = null,
+            float                   opacity    = 1f,
+            float                   rotation   = 0f, Vector2? origin = null,
+            SpriteEffects           effects    = SpriteEffects.None,
+            float                   layerDepth = 0f);
     }
 
     public interface IFontGlyph
     {
-        char Character { get; }
-        ITexture2D Texture { get; }
-        float Width { get; }
-        float Height { get; }
+        char       Character { get; }
+        ITexture2D Texture   { get; }
+        float      Width     { get; }
+        float      Height    { get; }
     }
 }
