9cd4f8cd2d444c182e3c800e5422fe051fb8e8c9
Kenny van Vulpen <kennyvvulpen@ziggo.nl>
2021-10-30 15:29:33 +0200

    Fix small merge issue

--- src/RocketUI/Serialization/Xaml/RocketXamlLoader.cs
+++ src/RocketUI/Serialization/Xaml/RocketXamlLoader.cs
@@ -233,35 +233,4 @@ namespace RocketUI.Serialization.Xaml
 			return Load<TBaseInstance>(GetStream(typeof(TXamlType)), instance);
 		}
 	}
-
-	public static class RocketXamlSaver
-	{
-		public static string SaveToXaml<T>(T instance)
-		{
-			var       sb        = new StringBuilder();
-			using var writer    = new StringWriter(sb);
-			using var xmlWriter = new XmlTextWriter(writer);
-			xmlWriter.Formatting = Formatting.Indented;
-			xmlWriter.Indentation = 2;
-			SaveToXaml(instance, xmlWriter);
-			return sb.ToString();
-		}
-		
-		public static void SaveToXaml<T>(T instance, XmlWriter xmlWriter)
-		{
-			var writerSettings = new XamlXmlWriterSettings();
-			using var xamlXmlWriter  = new XamlXmlWriter(xmlWriter, RocketXamlLoader.Context, writerSettings);
-			SaveToXaml(instance, xamlXmlWriter);
-		}
-		
-		public static void SaveToXaml<T>(T instance, XamlXmlWriter xamlXmlWriter)
-		{
-			var readerSettings = new XamlObjectReaderSettings();
-			readerSettings.IgnoreDefaultValues = true;
-			readerSettings.UseIgnoreDataMemberAttribute = true;
-			using var reader = new XamlObjectReader(instance, RocketXamlLoader.Context, readerSettings);
-			
-			XamlServices.Transform(reader, xamlXmlWriter);
-		}
-	}
 }
